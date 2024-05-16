2100adfd8a06ff96af3e3ee52d0a2522ad8fb7a4
Kharis Fadillah <kharisf@gmail.com>
2024-05-16 15:30:38 +0800

    add activity log, fix seeder, etc

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class User extends Authenticatable
 {
