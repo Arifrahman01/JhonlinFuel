9f3117885deb32a278bcc9c9ee1548da790469df
Arif Rahman <arfrahmann@gmail.com>
2024-05-28 10:38:21 +0800

    activity

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Transaction\TmpTransaction;
+use App\Models\Transaction\Transaction;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Activity extends BaseModel
@@ -11,5 +13,16 @@ class Activity extends BaseModel
     public function company() :BelongsTo
     {
         return $this->belongsTo(Company::class);
+    }
+
+    public function hasData(): bool
+    {
+        if (TmpTransaction::where('activity', $this->activity_code)->exists()) {
+            return true;
+        }
+        if (Transaction::where('activity_id', $this->id)->exists()) {
+            return true;
+        }
+        return false;
     }
 }
