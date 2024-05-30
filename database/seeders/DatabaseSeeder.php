776c415f52ce8f61d125d1b83b94d5ae717df739
Kharis Fadillah <kharisf@gmail.com>
2024-05-30 09:17:49 +0800

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,27 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Activity;
-use App\Models\Company;
-use App\Models\Department;
-use App\Models\Equipment;
-use App\Models\Fuelman;
-use App\Models\Issue\IssueDetail;
-use App\Models\Issue\IssueHeader;
-use App\Models\Material\Material;
-use App\Models\Material\MaterialStock;
-use App\Models\Menu;
-use App\Models\Period;
-use App\Models\Permission;
-use App\Models\Plant;
-use App\Models\Role;
-use App\Models\Sloc;
-use App\Models\Uom;
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
