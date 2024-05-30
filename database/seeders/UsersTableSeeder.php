881070387b9a03b2ec7d9f66212ad33c127fb5e8
Kharis Fadillah <kharisf@gmail.com>
2024-05-30 09:20:06 +0800

    fix seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,5 +16,18 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         //
+        $role = Role::create([
+            'role_code' => 'sa',
+            'role_name' => 'Super Administrator',
+            'notes' => 'JANGAN DIEDIT MAUPUN DIHAPUS, INI ROLE PALING SAKTI'
+        ]);
+
+        $user = User::create([
+            'name' => 'Super Administrator',
+            'username' => 'sa',
+            'password' => Hash::make('Jhonlin@123'),
+        ]);
+
+        $user->roles()->attach($role->id);
     }
 }
